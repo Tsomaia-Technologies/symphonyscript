2d1755d419e1937dda1bf9c9cc87d64b6d9d6268
Tornike Tsomaia <ttsomaia@emis.ge>
2025-12-25 08:53:14 +0400

    fixes

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,37 @@
-const nxPreset = require('@nx/jest/preset').default;
+const fs = require('fs');
+const path = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest');
 
-module.exports = { ...nxPreset };
+// Load your base tsconfig to automatically map paths (e.g. @symphonyscript/kernel)
+const tsconfig = require('./tsconfig.base.json');
+
+module.exports = {
+    // 1. Use SWC for blazing fast transforms (20x faster than ts-jest)
+    transform: {
+        '^.+\\.(t|j)sx?$': ['@swc/jest', {
+            jsc: {
+                target: 'es2022',
+                parser: {
+                    syntax: 'typescript',
+                    decorators: true,
+                    dynamicImport: true,
+                },
+                transform: {
+                    legacyDecorator: true,
+                    decoratorMetadata: true,
+                },
+            },
+        }],
+    },
+
+    // 2. Automatically map your aliases (@symphonyscript/* -> packages/*/src)
+    moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+        prefix: '<rootDir>/'
+    }),
+
+    // 3. Standard clean-up
+    moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+    testEnvironment: 'node',
+    // Stop Jest from trying to test your dist folders
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
